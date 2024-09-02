5a14fd5f3c2d64a2a0f3d608f7516d206a79f960
rakeshyadav36k <110667423+rakeshyadav36k@users.noreply.github.com>
2024-09-02 16:42:49 +0530

    Update Longest_substring_without_repeating_character.cpp

--- Longest_substring_without_repeating_character.cpp
+++ Longest_substring_without_repeating_character.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int ans = 0;
+        int right = 0,left = 0;
+        unordered_map<char,int> m;
+        while(right < n)
+        {
+            if(m.find(s[right]) != m.end())
+                left = max(left, m[s[right]] + 1); // if any char repeated then move left pointer
+ 
+            m[s[right]] = right; // putting the index where last time it appears
+            ans = max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+};
+
 int main(){
-    cout<<"Hello world";
+    string s;
+    cout << "Enter a string: ";
+    getline(cin, s);
+
+    Solution solution;
+    int maxLength = solution.lengthOfLongestSubstring(s);
+
+    cout << "Length of the longest substring without repeating characters: " << maxLength << endl;
     return 0;
 }
